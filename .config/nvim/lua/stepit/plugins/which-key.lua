45398fa9031067b78c9338ab358f90c930511e63
stepit <stefanofrancesco.pitton@gmail.com>
2024-10-09 10:30:32 +0200

    update which-key delay

--- .config/nvim/lua/stepit/plugins/which-key.lua
+++ .config/nvim/lua/stepit/plugins/which-key.lua
@@ -3,6 +3,7 @@ return {
   name = "WhichKey",
   event = "VimEnter",
   opts = {
+    delay = 500,
     icons = {
       breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
       separator = "→", -- symbol used between a key and it's label
