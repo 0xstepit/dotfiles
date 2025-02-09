d3abc7947746632db6bbe7af5ed4cde8b4bd0582
stepit <stefanofrancesco.pitton@gmail.com>
2025-02-09 09:42:01 +0100

    fix gitlinker config

--- .config/nvim/lua/stepit/plugins/gitlinker.lua
+++ .config/nvim/lua/stepit/plugins/gitlinker.lua
@@ -1,12 +1,14 @@
 return {
   "ruifm/gitlinker.nvim",
-  lazy=true,
+  -- lazy = true,
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    local gitlinker = require "gitlinker"
+    local gitlinker = require("gitlinker")
 
-    gitlinker.setup {}
+    gitlinker.setup({})
 
-    vim.keymap.set("n", "<leader>gy", function() gitlinker.get_buf_range_url() end, { desc = "[Y]ank repository link" })
+    vim.keymap.set("n", "<leader>gy", function()
+      gitlinker.get_buf_range_url()
+    end, { desc = "[Y]ank repository link" })
   end,
 }
