c7a695b89459b15f325765687248af7443bb1a44
stepit <stefanofrancesco.pitton@gmail.com>
2025-02-09 09:40:48 +0100

    improve ui with autocmd

--- .config/nvim/lua/stepit/autocommands.lua
+++ .config/nvim/lua/stepit/autocommands.lua
@@ -12,18 +12,33 @@ vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("stepit/close_with_q", { clear = true }),
   desc = "Close with <q>",
   pattern = {
+    "TelescopePrompt",
     "git",
     "fugitive",
     "help",
     "man",
     "qf",
-    "query",
-    "scratch",
   },
   callback = function(args)
     vim.keymap.set("n", "q", "<cmd>quit<cr>", { buffer = args.buf, noremap = true })
   end,
 })
+
+-- vim.api.nvim_create_autocmd("FileType", {
+--   group = vim.api.nvim_create_augroup("stepit/close_with_q", { clear = true }),
+--   desc = "Close with <q>",
+--   pattern = {
+--     "fugitive",
+--     "help",
+--     "man",
+--     "qf",
+--     "query",
+--     "scratch",
+--   },
+--   callback = function(args)
+--     vim.keymap.set("n", "q", "<cmd>quit!<cr>", { buffer = args.buf })
+--   end,
+-- })
 
 -- https://github.com/MariaSolOs/dotfiles/blob/main/.config/nvim/lua/autocmds.lua#L53-L63
 vim.api.nvim_create_autocmd("BufReadPost", {
@@ -42,58 +57,34 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
   command = "set formatoptions-=cro",
 })
 
-vim.api.nvim_create_autocmd("WinEnter", {
-  pattern = {
-    "*.lua",
-    "*.go",
-    "*.md",
-  },
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "TelescopePrompt", "fugitive", "git", "qf", "GV" },
+  desc = "Set cursorline to true when entering a window.",
   callback = function()
-    vim.wo.cursorline = true
+    vim.wo.cursorline = false
+    vim.wo.colorcolumn = ""
+    -- vim.wo.spell = false
   end,
 })
 
-vim.api.nvim_create_autocmd("WinLeave", {
-  pattern = {
-    "*.lua",
-    "*.go",
-    "*.md",
-  },
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  callback = function()
+    local exc = { "TelescopePrompt", "fugitive", "git", "qf", "GV" }
+    local valid = true
+    for _, e in ipairs(exc) do
+      if vim.bo.filetype == e then
+        valid = false
+      end
+    end
+
+    if valid then
+      vim.wo.cursorline = true
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "WinLeave" }, {
   callback = function()
     vim.wo.cursorline = false
   end,
 })
-
-vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("stepit/close_with_q", { clear = true }),
-  desc = "Close with <q>",
-  pattern = {
-    "TelescopePrompt",
-    "help",
-    "man",
-    "qf",
-    "query",
-    "scratch",
-  },
-  callback = function(args)
-    vim.keymap.set("n", "q", "<cmd>quit!<cr>", { buffer = args.buf })
-  end,
-})
-
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "TelescopePrompt",
---   desc = "Set cursorline to false when leaving a window.",
---   callback = function()
---       -- vim.wo.cursorline = false
---   end,
--- })
---
---
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = { "fugitive", "git", "qf", "GV"},
---   desc = "Set cursorline to true when entering a window.",
---   callback = function()
---       vim.wo.colorcolumn = ""
---             -- vim.wo.spell = false
---   end,
--- })
