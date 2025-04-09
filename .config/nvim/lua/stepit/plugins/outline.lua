edf4dd35e85a24696ca98dc8f85cee09c17d6274
stepit <stefanofrancesco.pitton@gmail.com>
2025-04-09 08:51:55 +0200

    feat: add outline default

---
+++ .config/nvim/lua/stepit/plugins/outline.lua
@@ -0,0 +1,11 @@
+return {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = {
+    { "<leader>mo", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
+  opts = {
+    -- Your setup opts here
+  },
+}
