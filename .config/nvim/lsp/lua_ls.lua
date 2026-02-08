760c62a8de4a7bb8113676de2679484b5413acf0
stepit <stefanofrancesco.pitton@gmail.com>
2026-02-08 15:46:56 +0100

    chore: lua lsp update

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -6,7 +6,6 @@ return {
 	settings = {
 		Lua = {
 			completion = { callSnippet = "Replace" },
-			-- Using stylua for formatting.
 			format = { enable = true },
 			hint = {
 				enable = true,
@@ -15,14 +14,14 @@ return {
 			runtime = {
 				version = "LuaJIT",
 			},
+			diagnostics = {
+				globals = { "vim" },
+			},
 			workspace = {
 				checkThirdParty = false,
-				library = vim.api.nvim_get_runtime_file("", true), -- Make LSP aware of Neovim runtime
-				-- library = vim.list_extend({
-				-- 	vim.env.VIMRUNTIME,
-				-- 	"${3rd}/luv/library",
-				-- 	-- Include Neovim runtime to navigate plugins.
-				-- }, vim.api.nvim_get_runtime_file("", true)),
+				library = {
+					vim.env.VIMRUNTIME,
+				},
 			},
 		},
 	},
